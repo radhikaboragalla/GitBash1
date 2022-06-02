3f3edcadc0700f0f4f3ed03e126797261c20c0e4
radhikaboragalla <radhika.radhi@gmail.com>
2022-06-02 01:07:43 -0400

    updated by RAD and conflict handled

--- src/test/java/com/gitBash/GitBash1Test.java
+++ src/test/java/com/gitBash/GitBash1Test.java
@@ -4,7 +4,19 @@ import org.testng.annotations.Test;
 
 public class GitBash1Test {
 	@Test
+<<<<<<< HEAD
        // commited by p1
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+         // hi
+=======
+       // commited by p1
+>>>>>>> 50758caffaf261d73e275cb9bc6b9cf50a1c399c
+=======
+       // commited by p1
+>>>>>>> 50758caffaf261d73e275cb9bc6b9cf50a1c399c
+>>>>>>> 56e8fc26e94df5fa4945e84b76a99c55040fc6ce
 	public void gitBash1Test()
 	{
 		System.out.println("Welcome");
--- src/test/java/com/gitBash/GitBash2Test.java
+++ src/test/java/com/gitBash/GitBash2Test.java
@@ -4,10 +4,19 @@ import org.testng.annotations.Test;
 
 public class GitBash2Test {
 	@Test
+            // comment by p1
 	public void gitBash2Test()
 	{
 		System.out.println("hi");
+<<<<<<< HEAD
                      // comment by person2
+=======
+<<<<<<< HEAD
+                   
+=======
+                     // comment by person2
+>>>>>>> 50758caffaf261d73e275cb9bc6b9cf50a1c399c
+>>>>>>> 56e8fc26e94df5fa4945e84b76a99c55040fc6ce
 	}
 
 }
